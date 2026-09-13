40f4dc7d13dbcd3ebffcac96bdf51dd61fe5fd41
Utpal Sonowal <sonowalu73@gmail.com>
2026-09-13 20:08:02 +0530

    added new project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -18,7 +18,7 @@ export const projects = [
      {
           title: "The Moment Studio",
           description:
-               "The website focuses on creating a premium visual experience while keeping the interface clean, responsive, and easy to navigate. It includes a photography-focused hero section, services, gallery, pricing packages, client testimonials, FAQs, contact CTA, and a complete footer.",
+               "The website focuses on creating a premium visual experience while keeping the interface clean, responsive, and easy to navigate.",
           image: "/project2.png",
           live: "https://the-moment-studio.vercel.app/",
           github: "https://github.com/Utpalsonowal7/The-Moment-Studio",
